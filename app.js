1a4efd3e0c18b7299df4d08b548092461d6c8243
Anirudh Sharma <126901961+anisharma07@users.noreply.github.com>
2024-01-09 13:44:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-"use strict";
 const express = require("express");
 const path = require("path");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
 const app = express();
 const server = createServer(app);
+const PORT = process.env.PORT || 3000;
 
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -65,11 +65,14 @@ function userLeave(id) {
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     const user = getCurrentUser(socket.id);
-    socket.emit("my chat message", msg, user.username, user.uniqueUserId);
+    const nameOfUser = user.username;
+    const userId = user.uniqueUserId;
+    socket.emit("my chat message", { msg, nameOfUser, userId });
     socket.broadcast
       .to(user.room)
-      .emit("chat message", msg, user.username, user.uniqueUserId);
+      .emit("chat message", { msg, nameOfUser, userId });
   });
+
   socket.on("create room", ({ pseudoUsername }) => {
     const roomId = getUniqueRoomId();
     const playerId = getUniqueUsername(roomId);
@@ -141,6 +144,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
+server.listen(PORT, () => {
+  console.log("server running at PORT: " + PORT);
 });
